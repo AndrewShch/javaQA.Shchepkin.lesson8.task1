f6dc87bfc6d0c5a376846e09a980716a8b22c6be
Andrey Shchepkin <lovenfed@gmail.com>
2021-07-12 21:16:21 +0300

    added tests

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,17 +1,69 @@
 package ru.netology;
 
 public class Radio {
+    private String name;
     private int currentStation;
-    private int maxStation;
-    private int minStation;
+    private int currentVolume;
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int maxVolume = 10;
+    private int minVolume = 0;
+    private boolean on;
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+
+    public int getMinStation() {
+        return minStation;
+    }
+    public void setMinStation(int minStation) {
+        this.minStation = minStation;
+    }
+
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+    public void setMaxStation(int maxStation) {
+        this.maxStation = maxStation;
+    }
 
 
 
     public void setCurrentStation(int currentStation){
-        if (currentStation < 0) {
+        if (currentStation < minStation) {
             return;
         }
-            if (currentStation > 9){
+            if (currentStation > maxStation){
                 return;
             }
             this.currentStation = currentStation;
@@ -19,6 +71,51 @@ public class Radio {
 
     public int getCurrentStation(){
         return currentStation;
-
     }
+
+    public void increaseStation() {
+        if (currentStation >= maxStation) {
+            setCurrentStation(minStation);
+        }
+        else {
+            currentStation = currentStation + 1;
+        }
+    }
+    public void downStation(){
+        if (currentStation <= minStation) {
+            setCurrentStation(maxStation);
+        }
+        else {
+            currentStation = currentStation -1;
+        }
+    }
+    public void setCurrentVolume(int currentVolume){
+        if (currentVolume < minVolume){
+            return;
+        }
+        if (currentVolume > maxVolume){
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+    public int getCurrentVolume(){
+        return currentVolume;
+    }
+    public void increaseVolume() {
+        if (currentVolume >= maxVolume) {
+            setCurrentVolume(minVolume);
+        }
+        else {
+            currentVolume = currentVolume + 1;
+        }
+    }
+    public void downVolume(){
+        if (currentVolume <= minVolume) {
+            setCurrentVolume(maxVolume);
+        }
+        else {
+            currentVolume = currentVolume -1;
+        }
+    }
+
 }
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -3,23 +3,86 @@ package ru.netology;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class RadioTest {
+    Radio rad = new Radio();
     @Test
-    void shouldNotSetStationBelowMin() {
-        Radio rad = new Radio();
-
-        rad.setCurrentStation(0);
-
-        rad.setCurrentStation(-1);
-        assertEquals(0,rad.getCurrentStation());
+    public void createRadio() {
+        String expected = "Radio";
+        assertNull(rad.getName());
+        rad.setName(expected);
+        assertEquals(expected, rad.getName());
     }
     @Test
-    void shouldNotSetStationBelowMax() {
-        Radio rad = new Radio();
+    void InitFields(){
+        assertEquals(9, rad.getMaxStation());
+        assertEquals(0, rad.getMinStation());
+        assertEquals(10, rad.getMaxVolume());
+        assertEquals(0, rad.getMinVolume());
+        assertFalse(rad.isOn());
+    }
+
+    ///Тестирование станций радио
+    @Test
+    void shouldNotSetStationBelowMin() {
+        rad.setCurrentStation(5);
+
+        rad.setCurrentStation(-1);
+        assertEquals(5, rad.getCurrentStation());
+    }
+
+    @Test
+    void shouldNotSetStationAboveMax() {
         rad.setCurrentStation(9);
 
         rad.setCurrentStation(10);
-        assertEquals(9,rad.getCurrentStation());
+        assertEquals(9, rad.getCurrentStation());
     }
+    @Test
+    void increaseStation(){
+        rad.setCurrentStation(7);
+
+        rad.increaseStation();
+        assertEquals(8,rad.getCurrentStation());
+    }
+    @Test
+    void downStation(){
+        rad.setCurrentStation(2);
+
+        rad.downStation();
+        assertEquals(1,rad.getCurrentStation());
+    }
+
+    ///Тестирование громкости радио
+    @Test
+    void shouldNotSetVolumeBelowMin() {
+        rad.setCurrentVolume(5);
+
+        rad.setCurrentVolume(-1);
+        assertEquals(5, rad.getCurrentVolume());
+    }
+    @Test
+    void shouldNotSetVolumeAboveMax() {
+        rad.setCurrentVolume(5);
+
+        rad.setCurrentVolume(11);
+        assertEquals(5, rad.getCurrentVolume());
+    }
+    @Test
+    void increaseVolume(){
+        rad.setCurrentVolume(8);
+
+        rad.increaseVolume();
+        assertEquals(9,rad.getCurrentVolume());
+    }
+    @Test
+    void downVolume(){
+        rad.setCurrentVolume(5);
+
+        rad.downVolume();
+        assertEquals(4,rad.getCurrentVolume());
+    }
+
 }
